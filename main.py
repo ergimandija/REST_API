b65c93f24d580df2ceaf1b2d8a57d62336c38c81
R31-py <rei.kanani2006@gmail.com>
2025-04-01 14:56:44 +0200

    Verbindung zu Datenbank

--- main.py
+++ main.py
@@ -1,13 +1,49 @@
-from fastapi import FastAPI
+import os
+from fastapi import FastAPI, HTTPException, Depends
+from pydantic import BaseModel
+from typing import List
+import databases
+import sqlalchemy
+
+DATABASE_URL = os.environ.get("DATABASE_URL", "mysql+pymysql://ergimandija:!Insy_2023$@htl-projekt.com:3306/2024_4bx_ergimandija_recipeplatform")
+database = databases.Database(DATABASE_URL)
+metadata = sqlalchemy.MetaData()
+
+recipes_table = sqlalchemy.Table(
+    "recipes",
+    metadata,
+    sqlalchemy.Column("id", sqlalchemy.Integer, primary_key=True),
+    sqlalchemy.Column("name", sqlalchemy.String),
+)
+
+ingredients_table = sqlalchemy.Table(
+    "ingredients",
+    metadata,
+    sqlalchemy.Column("id", sqlalchemy.Integer, primary_key=True),
+    sqlalchemy.Column("name", sqlalchemy.String),
+)
+
+engine = sqlalchemy.create_engine(DATABASE_URL)
+metadata.create_all(engine)
 
 app = FastAPI()
 
+class Ingredient(BaseModel):
+    name: str
+    type: str
+    r_id: int
 
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
+class Recipe(BaseModel):
+    name: str
+    description: str
+    steps: str
+    ingredients: List[int]
 
+@app.on_event("startup")
+async def startup():
+    await database.connect()
 
-@app.get("/hello/{name}")
-async def say_hello(name: str):
-    return {"message": f"Hello {name}"}
+@app.on_event("shutdown")
+async def shutdown():
+    await database.disconnect()
+
